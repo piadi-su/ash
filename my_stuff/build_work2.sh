6dc2f2de91399a44402cf929993613a715af0586
piadi-su <213212422+piadi-su@users.noreply.github.com>
2026-06-17 23:10:42 +0200

    remove extra stuff 2

--- my_stuff/build_work2.sh
+++
@@ -1,1 +0,0 @@
-gcc work_2.c $(pkg-config --cflags --libs xft x11) -o bar
--- my_stuff/config.h
+++
@@ -1,7 +0,0 @@
-/* config.h */
-
-// Altezza della barra in pixel
-#define BAR_HEIGHT 24
-
-// Nome del font moderno (Fontconfig format)
-#define BAR_FONT "Iosevka Nerd Font:size=13:antialias=true"
